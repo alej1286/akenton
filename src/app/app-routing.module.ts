5ec0d865d4dc9fdd8369c358eb3087722d1a5d5f
alej1286 <alej1286@gmail.com>
2021-12-26 14:59:35 -0500

    fxlayout fixed for controls form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
-import { DefaultComponent } from './Components/default/default.component';
 import { OrderComponent } from './Components/order/order.component';
 
 const routes: Routes = [
